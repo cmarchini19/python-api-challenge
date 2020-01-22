c51f2f01e19d43af8e29765e58a21c7cf21f2fc1
Chris Marchini <cmarchini@rti.org>
2020-01-22 12:18:04 -0500

    Updated API info

--- Marchini-Analysis/api_keys.py
+++ Marchini-Analysis/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "96e634bb17db1387c6eb44bc6d0696cb"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBALjBRG2dyPgh5QZnEHeHADnIoj9OxdwQ"
