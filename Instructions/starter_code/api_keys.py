e2d2c63f8905e293dbba51517fb9f6542c9c3213
Chris Marchini <cmarchini@rti.org>
2020-01-22 17:19:35 -0500

    Updated Weather assignment
    updates notebook from meeting with tutor

--- Instructions/starter_code/api_keys.py
+++ Instructions/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "96e634bb17db1387c6eb44bc6d0696cb"
+weather_api_key = "1ad2bb6d0fe9d9719b64dc3ec4578d1f"
 
 # Google API Key
-g_key = "AIzaSyBALjBRG2dyPgh5QZnEHeHADnIoj9OxdwQ"
+#g_key = "AIzaSyBALjBRG2dyPgh5QZnEHeHADnIoj9OxdwQ"
