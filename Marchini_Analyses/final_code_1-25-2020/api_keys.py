e130c41b5a8772967e64b477d6cdbe03a7703b02
Chris Marchini <cmarchini@rti.org>
2020-01-25 21:29:25 -0500

    Sanitized API Keys
    Dropped out API keys

--- Marchini_Analyses/final_code_1-25-2020/api_keys.py
+++ Marchini_Analyses/final_code_1-25-2020/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "1ad2bb6d0fe9d9719b64dc3ec4578d1f"
+weather_api_key = "YOUR KEY HERE"
 
 # Google API Key
-#g_key = "AIzaSyBALjBRG2dyPgh5QZnEHeHADnIoj9OxdwQ"
+#g_key = "YOUR KEY HERE"
